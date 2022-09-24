19d762b66ca5de968acc24795c11e50fbfc277a1
Luis Alejandro Peña <luisalejandropena425@gmail.com>
2022-09-23 23:45:52 -0500

    navbar-responsive

--- portafolio-luis-alejandro/README.md
+++ portafolio-luis-alejandro/README.md
@@ -68,3 +68,12 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `yarn build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+### To do
+
+- estoy trabajando en el navbar
+- revisar responsive del navbar
+- hacer icono pequeño. solo la piña
+
--- portafolio-luis-alejandro/src/components/Navbar.js
+++ portafolio-luis-alejandro/src/components/Navbar.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./styles/Navbar.css";
 import logo from "../assets/logoPortafolio1.png";
-export const Navbar = () => {
-  return (
-    <nav className="navContainer">
-      <div className="itemsContainer">
-        <div className="leftNav">
-          <Link to="/">
-            <img className="logoNav" src={logo} alt="logo" />
-          </Link>
-        </div>
-      </div>
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
-      <div className="rightNav">
-       
+export const Navbar = () => {
+
+  const { height, width } = useWindowDimensions();
+  const [path, setPath] = useState(window.location.pathname);
+
+  const DesktopNav = () => {
+    return (
+      <nav className="navContainer">
+        <div className="itemsContainer">
+          <div className="leftNav">
+            <Link to="/">
+              <img className="logoNav" src={logo} alt="logo" />
+            </Link>
+          </div>
+        </div>
+
+        <div className="rightNav">
+
           <NavLink
             activeClassName="active"
             className="linkNav"
@@ -48,8 +55,71 @@ export const Navbar = () => {
           >
             Contact
           </NavLink> */}
-        
-      </div>
-    </nav>
+
+        </div>
+      </nav>
+    )
+  }
+  const MobileNav = () => {
+    return (
+      <nav role="navigation">
+        <div className="menuToggle">
+
+          <input type="checkbox" />
+
+
+          <span></span>
+          <span></span>
+          <span></span>
+
+
+          <div className="menu">
+            <div className="containerLink">
+              <NavLink
+                activeClassName="active"
+                className="linkNav"
+                exact
+                to="/"
+                onClick={() => setPath('/')}
+              >
+                Home
+              </NavLink>
+              {path === '/' && <img className="logoNav" src={logo} alt="logo" />}
+            </div>
+            <div className="containerLink">
+              <NavLink
+                activeClassName="active"
+                className="linkNav"
+                exact
+                to="/my-work"
+                onClick={() => setPath('/my-work')}
+              >
+                My Work
+              </NavLink>
+              {path === '/my-work' && <img className="logoNav" src={logo} alt="logo" />}
+            </div>
+            <div className="containerLink">
+              <NavLink
+                activeClassName="active"
+                className="linkNav"
+                exact
+                to="/projects"
+                onClick={() => setPath('/projects')}
+              >
+                Projects
+              </NavLink>
+              {path === '/projects' && <img className="logoNav" src={logo} alt="logo" />}
+            </div>
+
+          </div>
+        </div>
+      </nav>
+    )
+  }
+  return (
+    <>
+      {width >= '844' ? <DesktopNav /> : <MobileNav />}
+      {width}
+    </>
   );
 };
