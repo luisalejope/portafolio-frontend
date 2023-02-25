1a3daa640001c09b98d92593a743430216c76a17
Luis Alejandro Peña <luisalejandropena425@gmail.com>
2023-02-25 01:32:58 -0500

    skills info changed

--- src/components/home-components/SkillsPPT/SkillsPPt.jsx
+++ src/components/home-components/SkillsPPT/SkillsPPt.jsx
@@ -13,7 +13,7 @@ const SkillsPPt = () => {
           <h3>
             As an adept Junior Developer, I have a well-rounded skill set in
             programming and honed talents in UX/UI updates and software testing,
-            making me an ideal fit for the Junior Front End Developer position.
+            making me an ideal fit for the Front End Developer position.
             Further, I am a recognized expert in teamwork and a communicative
             collaborator. My previous roles have strengthened my capabilities in
             innovation and motivation, including a keen attention to detail and
